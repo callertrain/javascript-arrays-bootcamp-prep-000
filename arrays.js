e92606fea65c635a269c694ae1d8ff493d966bb3
callertrain <cahcardeal@gmail.com>
2017-09-25 18:52:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand",
   "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(chocolateBars, 'foo'){
-  return ['foo', ...chocolateBars];
+function addElementToBeginningOfArray([1], 'foo'){
+  return [1, 'foo'];
 }
